d1b9d93e91ed43528555ae1bd7f17c7e31eb425a
Matheus Fonseca de Souza <matheus.fonseca@CONTABILIZEI.COM.BR>
2022-08-05 16:18:18 -0300

    app

---
+++ src/App.vue
@@ -0,0 +1,55 @@
+<script setup>
+import Item from "./components/Item.vue";
+</script>
+
+<script>
+import Dados from "./dados/services.json";
+export default {
+  data() {
+    return {
+      servicos: null,
+    };
+  },
+  mounted() {
+    this.servicos = Dados;
+  },
+};
+</script>
+
+<template>
+  <div class="logospace">
+    <a href="https://vuejs.org/" target="_blank">
+      <img style="width:70px;" src="./assets/vue.svg" class="logo vue" alt="Vue logo" />
+    </a>
+    <h1>seja bem vindo!</h1>
+    <hr />
+    <div class="items">
+      <Item v-for="(servico, index) in servicos" :key="index" :item="servico" />
+    </div>
+  </div>
+</template>
+
+<style scoped>
+ 
+.logospace a{
+  display: flex;
+}
+.logo {
+  
+  margin: 0 auto;
+ height: 6em;
+  padding: 1.5em;
+  will-change: filter;
+}
+.logo:hover {
+  filter: drop-shadow(0 0 2em #646cffaa);
+}
+.logo.vue:hover {
+  filter: drop-shadow(0 0 2em #42b883aa);
+}
+.items{
+  display: flex;
+  flex-wrap: wrap;
+  justify-content: center;
+}
+</style>
---
+++ src/components/Item.vue
@@ -0,0 +1,47 @@
+<template>
+  <div class="card">
+    <a :href="item.href">
+      <img :src="item.img" alt="item do serviço" />
+    </a>
+    <span>
+      {{ item.titulo }}
+    </span>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["item"],
+};
+</script>
+
+<style scoped>
+div {
+  margin: 20px 50px 20px 50px;
+  display: block;
+  width: 100px;
+}
+img {
+  width: 80px;
+}
+span {
+  font-weight: 900;
+}
+.card{
+    min-width: 120px;
+    min-height: 150px;
+    display: flex;
+    justify-content: center;
+    flex-direction: column;
+    align-items: center;
+    border:1px solid gray;
+    border-radius: 5%;
+    box-shadow: 0 0 2em gray;
+    opacity: 0.9;
+    transition: .4s;
+}
+.card:hover{
+      box-shadow: 0 0 4em gray;
+    opacity: 1;
+}
+</style>
